da7eb48393334ef452cf6ed334922ebb3c016afb
unknown <montherismail90@gmail.com>
2022-03-09 10:52:48 +0200

    fix

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,5 @@
 const signUp = require('./signup');
 const login = require('./login');
+const addToCart = require('./addToCart');
 
-module.exports = { signUp, login };
+module.exports = { signUp, login, addToCart };
