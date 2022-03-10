a7aeb6e8f767a6eb6e019d69f8d3995979f6e80d
unknown <stm1998@hotmail.com>
2022-03-10 07:13:24 +0200

    logout and clear cookies

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  signUp, login, deleteFromCart, addToCart, renderCars,routProtector
+  signUp, login, deleteFromCart, addToCart, renderCars,routProtector, logout
 } = require('../controllers');
 
 router.post('/signup', signUp);
 router.post('/login', login);
 router.use(routProtector);
+router.get('/logout' , logout);
 router.get('/cars', renderCars);
 router.get('/myCart', addToCart);
 router.put('/delete', deleteFromCart);
